ecf43a52117c6ecb92acf46c868f0acf9974ec67
filipelion <filipelion@github.com>
2019-08-23 07:12:18 -0300

    Linear robot

--- robor.py
+++ robor.py
@@ -1,3 +1,11 @@
-entrada = input().upper()
-f_count = entrada.count("F")
-print(f_count - (len(entrada)- f_count))
+total_steps = input('Insert commands: \n').upper()
+f_steps = total_steps.count("F")
+final_steps = (f_steps - (len(total_steps)- f_steps))
+
+if final_steps == 0:
+    print("robot doesn't move")
+else:
+    if final_steps > 0:
+        print("robot walked %d steps"% final_steps)
+    else:
+        print("robot walked %d 'moonwagilker' steps"% (-final_steps)) #negative to positive with -variable
