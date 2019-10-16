52f6737dddce9b0e3281746b1e4653d3f14c5813
filipelion <filipelion@github.com>
2019-10-16 16:47:50 -0300

    Segunda versão

--- time-trial-ilhas.py
+++ time-trial-ilhas.py
@@ -1,19 +1,52 @@
-#numero de olhas e numero de cabos
-
 def create_graph():
-    ilhas, cabos = map(lambda i: int(i), input().split()) # fist line
-    graph = [[] for j in range(n)]
+    ilhas, cabos = map(lambda i: int(i), input().split())
+    matrix = [[0 for i in range(ilhas)] for j in range(ilhas)]
 
-    for j in range(ilhas-1):
-        u, v, p = map(lambda i: int(i), input().split())
-        graph[u-1].append((v-1, p))
-        graph[v-1].append((u-1, p))
+    for i in range(cabos):
+        if i < ilhas:
+            u, v, p = map(lambda i: int(i), input().split())
+            matrix[u - 1][v - 1] = p
+            matrix[v - 1][u - 1] = p
+        else:
+            trash = input()
 
-    servidor = int(input()) # inteiro S
+    servidor = int(input())
 
-    return (graph, ilhas, cabos, servidor)
+    return (matrix, ilhas, cabos, servidor)
 
-'''
-Depois que a função retorna a tupla com o grafo e a quantidade de ilhas, cabos e servidores.
-O proximo passo e traçar o caminho mais do nó com o maior e do menor ping do servidor percorrendo todos os nós atraves de uma busca em profundidade.
-'''
+def dijkstra_algorithm(matrix_adj, start, end):
+
+    D = {str(i): float("INF") for i in range(len(matrix_adj))}
+    D[str(start)] = 0  # starting with zero in dictionary
+    L = [(0, start)]  # make a list starting with zero (cost, vertice)
+
+    while L != []:
+        cost, current_vertice = L.pop(0)
+        for neighbor in range(len(matrix_adj)):
+            if matrix_adj[current_vertice][neighbor]:
+                if D[str(neighbor)] > (matrix_adj[current_vertice][neighbor] + cost):
+                    D[str(neighbor)] = matrix_adj[current_vertice][neighbor] + cost
+                    L.append((D[str(neighbor)], neighbor))
+    return D
+
+def ping(temp, island):
+    del temp[str(island)]
+    print(temp)
+    smaller_ping, bigger_ping = max(temp.values())
+    print(bigger_ping, smaller_ping)
+    return bigger_ping - smaller_ping
+
+# main function
+
+if __name__ == "__main__":
+    map_ilhas = create_graph()
+    list_servidor = ((map_ilhas[0][map_ilhas[-1] - 1])[:])
+    list_servidor.sort()
+
+    for i in list_servidor:
+        if i > 0:
+            smaller_ping = i
+            break
+
+    tracks = dijkstra_algorithm(map_ilhas[0], 0, map_ilhas[-1]-1)
+    print(max(tracks.values()) - smaller_ping)
