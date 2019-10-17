ace0554164b2d89a22f4a22d34a0085b1c32055b
filipelion <filipelion@github.com>
2019-10-16 23:13:59 -0300

    Versão ainda mal otimizada

--- time-trial-ilhas.py
+++ time-trial-ilhas.py
@@ -3,24 +3,22 @@ def create_graph():
     matrix = [[0 for i in range(ilhas)] for j in range(ilhas)]
 
     for i in range(cabos):
-        if i < ilhas:
-            u, v, p = map(lambda i: int(i), input().split())
-            matrix[u - 1][v - 1] = p
-            matrix[v - 1][u - 1] = p
-        else:
-            trash = input()
+        u, v, p = map(lambda i: int(i), input().split())
+        matrix[u - 1][v - 1] = p
+        matrix[v - 1][u - 1] = p
 
     servidor = int(input())
 
     return (matrix, ilhas, cabos, servidor)
 
-def dijkstra_algorithm(matrix_adj, start, end):
+def dijkstra_algorithm(matrix_adj, start):
 
     D = {str(i): float("INF") for i in range(len(matrix_adj))}
     D[str(start)] = 0  # starting with zero in dictionary
     L = [(0, start)]  # make a list starting with zero (cost, vertice)
 
     while L != []:
+        print(D)
         cost, current_vertice = L.pop(0)
         for neighbor in range(len(matrix_adj)):
             if matrix_adj[current_vertice][neighbor]:
@@ -29,17 +27,11 @@ def dijkstra_algorithm(matrix_adj, start, end):
                     L.append((D[str(neighbor)], neighbor))
     return D
 
-def ping(temp, island):
-    del temp[str(island)]
-    print(temp)
-    smaller_ping, bigger_ping = max(temp.values())
-    print(bigger_ping, smaller_ping)
-    return bigger_ping - smaller_ping
-
 # main function
 
 if __name__ == "__main__":
     map_ilhas = create_graph()
+
     list_servidor = ((map_ilhas[0][map_ilhas[-1] - 1])[:])
     list_servidor.sort()
 
@@ -48,5 +40,18 @@ if __name__ == "__main__":
             smaller_ping = i
             break
 
-    tracks = dijkstra_algorithm(map_ilhas[0], 0, map_ilhas[-1]-1)
-    print(max(tracks.values()) - smaller_ping)
+    tracks = dijkstra_algorithm(map_ilhas[0], map_ilhas[-1]-1)
+    #del tracks.values[map_ilhas[-1]-1]
+
+    #print(max(tracks.values()) - smaller_ping)
+    dist = tracks
+    INFTO = 1123456789
+    menor = INFTO
+    maior = -1
+    for u in dist.values():
+        d = u
+        if d != 0:
+            menor = min(menor, d)
+        if d != INFTO:
+            maior = max(maior, d)
+    print(maior - menor)
