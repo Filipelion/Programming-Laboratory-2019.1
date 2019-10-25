f4697a05afe5616416aa80c58ab878662420c843
filipelion <filipelion@github.com>
2019-10-25 15:10:01 -0300

    Recursividade e aeromoças

--- aeromocas.py
+++ aeromocas.py
@@ -1,3 +1,16 @@
+'''
+Aluno: Filipe de Freitas Lima
+Data: 23/10/19
+
+Resumo:
+
+Implementei o algoritmo de dijkstra utilizando listas de adjacências.
+Construi as classes Nó e fila. A classe fila duplamente encadeada possui os
+metodos push, pop e emputy. Com isso evitei de utilizar as funções da
+biblioteca padrão do python.
+'''
+
+
 class Node:
     def __init__(self, data, next=None, previous=None):
         self.__data = data
@@ -6,15 +19,19 @@ class Node:
 
     def getData(self):
         return self.__data
+
     def getNext(self):
         return self.__next
+
     def getPrevious(self):
         return self.__previous
 
     def setData(self, data):
         self.__data = data
+
     def setNext(self, next):
         self.__next = next
+
     def setPrevious(self, previous):
         self.__previous = previous
 
@@ -26,11 +43,13 @@ class Queue:
 
     def getHead(self):
         return self.__head
+
     def getLast(self):
         return self.__last
 
     def setHead(self, head):
         self.__head = head
+
     def setLast(self, last):
         self.__last = last
 
@@ -41,7 +60,6 @@ class Queue:
             self.__head.setPrevious(new_node)
         self.__head = new_node
 
-
     def my_pop(self):
         if self.__head is None:
             return None
@@ -51,28 +69,11 @@ class Queue:
             tmp.setPrevious(None)
             return tmp.getData()
 
-    def size(self):
-        tmp = self.__head
-        count = 0
-
-        while tmp is not None:
-            count += 1
-            tmp = tmp.getNext()
-        return count
-
     def isEmpty(self):
         if self.__head is None:
             return True
         else:
             return False
-
-    def printstack(self):
-
-        print("stack elements are:")
-        temp = self.getHead()
-        while temp is not None:
-            print(temp.getData(), end="->")
-            temp = temp.getNext()
 
 
 def split_to_int(sentence):
@@ -89,10 +90,10 @@ def split_to_int(sentence):
 
     return split_value
 
-def create_graph():
-    n, m = split_to_int(input()) # fist line
-    graph = [[] for j in range(n)]
 
+def create_graph():
+    n, m = split_to_int(input())  # fist line
+    graph = [[] for j in range(n)]
 
     for j in range(m):
         a, b, dist = split_to_int(input())
@@ -101,12 +102,12 @@ def create_graph():
 
     return (graph, n, m)
 
-def dijkstra_algorithm(vertex, start):
-    Q = Queue() # FILA
-    Q.push((0, start))
-    dist = [float("INF")]*city # FILA
-    dist[start] = 0
 
+def dijkstra_algorithm(vertex, start):
+    Q = Queue()
+    Q.push((0, start))
+    dist = [float("INF")] * city  # FILA
+    dist[start] = 0
 
     while not Q.isEmpty():
         cost, current_vertice = Q.my_pop()
@@ -117,17 +118,28 @@ def dijkstra_algorithm(vertex, start):
                 dist[neighbor[0]] = neighbor[1] + cost
                 Q.push((dist[neighbor[0]], neighbor[0]))
 
+    return dist
 
-
-    return max(dist)
 
 # main function
 if __name__ == "__main__":
     vertex, city, voos = create_graph()
 
-    menor = float("INF")
+    result = [0 for i in range(city)]
+
     for i in range(city):
-        result = dijkstra_algorithm(vertex, i)
-        if result < menor and result != 0:
-            menor = result
-    print(menor)
+        d = dijkstra_algorithm(vertex, i)
+
+
+        for j in range(city):
+            if result[j] < d[j]:
+                result[j] = d[j]
+
+
+
+    smaller = float("INF")
+    for i in result:
+        if i < smaller:
+            smaller = i
+
+    print(smaller)
