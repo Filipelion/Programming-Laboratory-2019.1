2b76685c6b36bd38eb8e1eb56047aa92caee4e70
filipelion <filipelion@github.com>
2019-10-27 10:13:58 -0300

    versão 2.0

--- O lobo mau e as ovelhas do Juvenal.py
+++ O lobo mau e as ovelhas do Juvenal.py
@@ -1,5 +1,13 @@
-import sys
-sys.setrecursionlimit(10000)
+'''
+Aluno: Filipe de Freitas Lima
+Data: 27/10/19
+
+Resumo:
+
+Implementei o algoritmo utilizando recursões;
+Construi as classes Matrix e search que possuem metodos para criar o campo e buscar as informações contidas.
+'''
+
 
 class Matrix:
     def __init__(self, num_rows, num_columns):
@@ -19,6 +27,57 @@ class Matrix:
             self.__matrix.insert(i, line)
 
         return self.__matrix
+
+class search(Matrix):
+    def __init__(self, num_rows, num_columns):
+        super().__init__(num_rows, num_columns) # class Matrix
+        self.__num_rows = num_rows
+        self.__num_columns = num_columns
+
+
+
+    def field(self, m, rows, columns):
+        global sheeps, wolves
+        num_ovelhas = 0
+        num_lobos = 0
+        for i in range(rows):
+            for j in range(columns):
+
+                if m[i][j] != '#':
+                    sheeps = 0
+                    wolves = 0
+                    self.bloody_night(m, i,j)
+                    if (sheeps or wolves) > 0:
+
+                        if sheeps > wolves:
+                            num_ovelhas += sheeps
+                        elif wolves >= sheeps:
+                            num_lobos += wolves
+
+        return num_ovelhas, num_lobos
+
+    def bloody_night(self, matrix, i, j):
+        global sheeps, wolves
+        value = matrix[i][j]
+
+        if value is '#':
+            return False
+
+        elif value is 'k':
+            sheeps += 1
+            matrix[i][j] = '#'
+
+        elif value is 'v':
+            wolves += 1
+            matrix[i][j] = '#'
+
+        matrix[i][j] = '#'  # to '.'
+
+        self.bloody_night(matrix, i, j + 1)  # go ahead
+        self.bloody_night(matrix, i, j - 1)  # go back
+        self.bloody_night(matrix, i - 1, j)  # go up
+        self.bloody_night(matrix, i + 1, j)  # go down
+
 
 
 def split_to_str(sentence):
@@ -44,52 +103,12 @@ def split_to_int(sentence):
     return split_value
 
 
-def bloody_night(matrix, i, j):
-    global ovelhas, lobos
-    value = matrix[i][j]
-
-    if value is '#':
-        return False
-
-    elif value is 'k':
-        ovelhas += 1
-        matrix[i][j] = '#'
-
-    elif value is 'v':
-        lobos += 1
-        matrix[i][j] = '#'
-
-    matrix[i][j] = '#' # to '.'
-
-    bloody_night(matrix, i, j + 1)  # go ahead
-    bloody_night(matrix, i, j - 1)  # go back
-    bloody_night(matrix, i - 1, j)  # go up
-    bloody_night(matrix, i + 1, j)  # go down
-
 
 # main function
 if __name__ == "__main__":
     row, col = split_to_int(input())
-    m1 = Matrix(row, col)
-    m1.insert_elements()
-    ma = m1.__repr__()
-
-    num_ovelhas = 0
-    num_lobos = 0
-    for i in range(row):
-        for j in range(col):
-
-            if ma[i][j] != '#':
-                ovelhas = 0
-                lobos = 0
-
-                bloody_night(ma, i, j)
-                if (ovelhas or lobos) > 0:
-
-                    if ovelhas > lobos:
-                        num_ovelhas += ovelhas
-                    elif lobos >= ovelhas:
-                        num_lobos += lobos
-
-
-    print(num_ovelhas, num_lobos)
+    m2 = search(row, col)
+    m2.insert_elements()
+    matrix = m2.__repr__()
+    sheeps, wolves = m2.field(matrix, row, col)
+    print(sheeps, wolves)
